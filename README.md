adbabb522632da73a978660a3c21c5af61fe6f6b
Jakub Molinski <kubamolinski@gmail.com>
2019-03-09 20:49:45 +0100

    setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # traktpy
+[![Build Status](https://travis-ci.org/jmolinski/traktpy.svg?branch=master)](https://travis-ci.org/jmolinski/traktpy)
+
 Python wrapper for Trakt REST api
---
+++ trakt/hooks.py
@@ -0,0 +1,17 @@
+import os
+
+
+def write_version(command):
+    version = command.egg_version
+
+    version_path = os.path.join(os.path.dirname(__file__), 'version.py')
+
+    with open(version_path, 'r') as fp:
+        lines = fp.read().split('\n')
+
+    for i, line in enumerate(lines):
+        if line.startswith('__version__ ='):
+            lines[i] = '__version__ = %r' % (version,)
+
+    with open(version_path, 'w') as fp:
+        fp.write('\n'.join(lines))
