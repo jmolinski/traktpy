45b7aa16b41ac50912482c19a4aed10d578c4eb1
Jakub Molinski <kubamolinski@gmail.com>
2019-03-24 17:05:20 +0100

    Add test data

---
+++ tests/test_data/movies.py
@@ -0,0 +1,26 @@
+MOVIE1 = {
+    "title": "Guardians of the Galaxy",
+    "year": 2014,
+    "ids": {
+        "trakt": 28,
+        "slug": "guardians-of-the-galaxy-2014",
+        "imdb": "tt2015381",
+        "tmdb": 118340,
+    },
+}
+
+MOVIE2 = {
+    "title": "Guardians of the Galaxy",
+    "year": 2014,
+    "ids": {
+        "trakt": 28,
+        "slug": "guardians-of-the-galaxy-2014",
+        "imdb": "tt2015381",
+        "tmdb": 118340,
+    },
+}
+
+MOVIE_PREMIERES = [
+    {"released": "2014-08-01", "movie": MOVIE1},
+    {"released": "2014-08-01", "movie": MOVIE2},
+]
